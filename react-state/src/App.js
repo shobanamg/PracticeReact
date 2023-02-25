15c0a2cdeb50853ecff0f4385cbc3fd532c37996
Shobana <mathiarulshobana@gmail.com>
2023-02-25 04:48:33 +0100

    here the function runs every time component rerender

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 
+const countInitial = () => {
+  console.log("It runs every time component rerender");
+  return 8;
+};
+
 function App() {
-  // Whenever we use useState hook, we need to pass the initial value. if we don't pass the initial value, the component will not rerender.
-  // In the mean time if we pass the initial value as a function,
-  // the function only run one time and it will not run every time the component rerender.
-  const [count, setCount] = useState(() => {
-    console.log("It render only one time");
-    return 8; // this is initial value of the  state.
-  });
+  const [count, setCount] = useState(countInitial());
 
   const decrementState = () => {
     setCount((prevState) => prevState - 1);
