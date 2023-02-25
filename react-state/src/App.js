6d84aa7db5e9ef3b4df395b8aa83b40e9ed38b7d
Shobana <mathiarulshobana@gmail.com>
2023-02-25 03:50:51 +0100

    refactored react state

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -4,14 +4,15 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(4);
   const decrementState = () => {
-    setCount(count - 1);
-    //if i want to decrement the value of the state every time by 2, It wont work here. It will decrease the value of the state
-    // every time by 1. so here we have to use the previous value of the state.
-    setCount(count - 1);
+    //if i want to decrement the value of the state every time by 2, It won't work here. It will decrease the value of the state
+    // every time by 1. so here we have to use the previous value of the state. Now it works.
+    // setCount(count - 1);
+    setCount((prevState) => prevState - 1);
+    setCount((prevState) => prevState - 1);
   };
 
   const incrementState = () => {
-    setCount(count + 1);
+    setCount((prevState) => prevState + 1);
   };
 
   return (
